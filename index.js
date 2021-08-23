fa82a19c1b52a2b3f417e9aea2af639243fc0f22
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-08-23 01:20:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Alex',
+    streetAddress: '5678 Smelly Baka St'
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, { [key]: value });
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+
+    return obj;
+}
